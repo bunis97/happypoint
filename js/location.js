f8e6d44dd5fed8a3c7c15d6a2faf65a5d697050d
bunis97 <78129275+bunis97@users.noreply.github.com>
2021-02-24 11:17:45 +0900

    Add files via upload

---
+++ js/location.js
@@ -0,0 +1,11 @@
+/*로케이션_MAP_*/
+$(function(){
+    // 메인메뉴 열기
+    $('.btn_toggle').on('click',function(){
+      $('#gnb').show();
+    });
+    // 메인메뉴 닫기
+    $('.btn_close').on('click',function(){
+      $('#gnb').hide();
+    });
+});
